56b96155ecc6155239ccb6614688e00117273857
Na Yeon <smilenayeon@naver.com>
2023-11-04 21:48:49 +0900

    Service description is done and descrption card is being added to DetailedServiceDescription.jsx.

---
+++ src/components/DetailedServiceDescription.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import DescriptionCard from './DescriptionCard';
+import Lightening from '../assets/images/lightening.svg';
+import PaperPlane from "../assets/images/paper-plane.svg";
+import Eye from "../assets/images/eye.svg";
+
+function DetailedServiceDescription() {
+    const cardData=
+        [
+            {img:Lightening,
+             alt:"lightening",
+             title:"Subscribe",
+             content:"Kickstart your design adventure by hopping on our monthly subscription. For just $2k, you get unlimited dibs on top-notch design work.",
+            },
+            {img:PaperPlane,
+             alt:"paper plane",
+             title:"Request",
+             content:"Alright, you're in! Now, it's time to toss your design tasks our way. Need a branding concept? A sleek landing page design? Or maybe a standout trifold brochure? No sweat. Just keep those requests coming!",
+            },
+            {img:Eye,
+             alt:"eye",
+             title:"Review",
+             content:"Hold tight! In just 48 hours, you'll get your first peek at your completed design. And if it's not love at first sight, no worries! We can tweak and tune it until it's just right - that's the beauty of unlimited revisions.",
+            },
+        ];
+  return (
+    <div className="DetailedServiceDescription">
+    <h1>How it works</h1>
+    <DescriptionCard />
+    <DescriptionCard/>
+    <DescriptionCard/>
+
+    </div>
+  )
+}
+
+export default DetailedServiceDescription;
--- src/components/ServiceDescription.jsx
+++ src/components/ServiceDescription.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import DesignRobot from "../assets/images/design-robot.jpg";
 
 function ServiceDescription() {
   return (
     <div className="service-description-section">
-     <h2>The future of design services</h2>
-
+    <img className="design-robot" src={DesignRobot} alt="design robot"/>
+     <div className="description">
+     <h2>The future of <br/> design services</h2>
+     <p>Need stunning graphic design, intuitive UI/UX, or eye-catching web design?  
+     Welcome home. We're Super Hello, your design partner for a flat monthly fee. 
+     We're all about delivering good creative in a new way.</p>
+     </div>
     </div>
   )
 }
