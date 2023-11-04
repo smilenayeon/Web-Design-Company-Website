471c58f3a3fa0fd8836cfe14b9be6a7bacdcf47d
Na Yeon <smilenayeon@naver.com>
2023-11-04 17:50:44 +0900

    NavBar basic structure and style is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import  '../../App.css';
+
+function NavBar() {
+  return (
+    
+    <div className="navbar">
+        <div className="navbar-logo">Super Hello</div>
+        <div className="navbar-links">
+            <a href="/benefits">Benefits</a>
+            <a href="/work">Recent Work</a>
+            <a href="/how-it-works">How It Works</a>
+            <a href="/pricing">Pricing</a>
+            <a href="/faqs">FAQs</a>
+            <a href="/contact">Contact</a>
+        </div>
+        <div className="navbar-login">
+            <button>Login</button>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar;
